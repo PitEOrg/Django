c837c77f77873c4cf72ded4d913c061850ff4133
Karol <hohner250@gmail.com>
2015-05-02 22:52:12 +0200

    .

--- systemocen/database/urls.py
+++ systemocen/database/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^signin/$', views.signin, name='signin'),
-	url(r'^student/$',views.studentpage, name='studentpage'),
-	url(r'^teacher/$',views.teacherpage, name='teacherpage'),
-	url(r'^teacher/subject/(?P<question_id>[0-9]+)$',views.teachersubject, name='teachersubject'),
+    url(r'^$', views.index, name='index'),
+    url(r'^signin/$', views.signin, name='signin'),
+    url(r'^student/$', views.studentpage, name='studentpage'),
+    url(r'^teacher/$', views.teacherpage, name='teacherpage'),
+    url(r'^teacher/subject/(?P<subject_id>[0-9]+)$', views.teachersubject, name='teachersubject'),
+    url(r'^student/subject/(?P<subject_id>[0-9]+)$', views.studentsubject, name='studentsubject'),
 ]
 
--- systemocen/database/views.py
+++ systemocen/database/views.py
@@ -4,84 +4,102 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from .models import Student, Teacher, Subject, SubjectsStudents
+
+
 def index(request):
-	return render(request, 'database/index.html')
-	
+    return render(request, 'database/index.html')
+
+
 def signin(request):
-	try:
-		log_step =  request.POST['step']
-	except KeyError:
-		return render(request, 'database/signin.html')
-	else:
-		if log_step == '1':
-			username = request.POST['username']
-			password = request.POST['password']
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					try:
-						student = Student.objects.get(user = request.user)
-						return HttpResponseRedirect(reverse('studentpage'))
-					except Student.DoesNotExist:
-						try:
-							teacher = Teacher.objects.get(user = request.user)
-							return HttpResponseRedirect(reverse('teacherpage'))
-						except Teacher.DoesNotExist:
-							return HttpResponse('Konto nie prydzielone')			
-				else:
-					return HttpResponse('Konto wylaczone')
-			else:
-				return HttpResponse('Bledny login')
-		else:
-			return HttpResponse('Cos' + log_step)
+    try:
+        log_step = request.POST['step']
+    except KeyError:
+        return render(request, 'database/signin.html')
+    else:
+        if log_step == '1':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    try:
+                        student = Student.objects.get(user=request.user)
+                        return HttpResponseRedirect(reverse('studentpage'))
+                    except Student.DoesNotExist:
+                        try:
+                            teacher = Teacher.objects.get(user=request.user)
+                            return HttpResponseRedirect(reverse('teacherpage'))
+                        except Teacher.DoesNotExist:
+                            return HttpResponse('Konto nie prydzielone')
+                else:
+                    return HttpResponse('Konto wylaczone')
+            else:
+                return HttpResponse('Bledny login')
+        else:
+            return HttpResponse('Cos' + log_step)
 
-	
 
-	
 def testpage(request):
-	if request.user.is_authenticated():
-		return HttpResponse("Zalogowany")
-	else:
-		return HttpResponse("Nie zalogowany")
-		
+    if request.user.is_authenticated():
+        return HttpResponse("Zalogowany")
+    else:
+        return HttpResponse("Nie zalogowany")
+
+
 def studentpage(request):
-	if request.user.is_authenticated():
-		try:
-			st = Student.objects.get(user = request.user)
-			subjects = (subjects.subject_id for subjects in st.subjectsstudents_set.all())
-			grades = []
-			for grade in st.grade_set.all():
-				grades.append([grade.subject_id.name, grade.value])
-			
-			return render(request, 'database/studentpage.html',{ 'student':st, 'grades':grades, 'subjects':subjects})
-		except Student.DoesNotExist:
-			return HttpResponse("Niema studenta")
-	else:
-		return HttpResponse("Nie zalogowany")		
-		
+    if request.user.is_authenticated():
+        try:
+            st = Student.objects.get(user=request.user)
+            subjects = (subjects.subject_id for subjects in st.subjectsstudents_set.all())
+            #grades = []
+            #for grade in st.grade_set.all():
+            #    grades.append([grade.subject_id.name, grade.value, grade.teacher_id])
+
+            return render(request, 'database/studentpage.html', {'student': st, 'subjects': subjects})
+        except Student.DoesNotExist:
+            return HttpResponse("Niema studenta")
+    else:
+        return HttpResponse("Nie zalogowany")
+
+
 def teacherpage(request):
-	if request.user.is_authenticated():
-		try:
-			te = Teacher.objects.get(user = request.user)
-			subjects = (subjects for subjects in te.subject_set.all())
-						
-			return render(request, 'database/teacherpage.html',{ 'teacher':te, 'subjects':subjects})
-		except Student.DoesNotExist:
-			return HttpResponse("Niema nayczyciela")
-	else:
-		return HttpResponse("Nie zalogowany")	
-		
-def teachersubject(request):
-	if request.user.is_authenticated():
-		try:
-			te = Teacher.objects.get(user = request.user)
-			subjects = (subjects for subjects in te.subject_set.all())
-						
-			return render(request, 'database/teacherpage.html',{ 'teacher':te, 'subjects':subjects})
-		except Student.DoesNotExist:
-			return HttpResponse("Niema nayczyciela")
-	else:
-		return HttpResponse("Nie zalogowany")	
-	
-	
+    if request.user.is_authenticated():
+        try:
+            te = Teacher.objects.get(user=request.user)
+            subjects = (subjects for subjects in te.subject_set.all())
+
+            return render(request, 'database/teacherpage.html', {'teacher': te, 'subjects': subjects})
+        except Teacher.DoesNotExist:
+            return HttpResponse("Nie ma nauczyciela")
+    else:
+        return HttpResponse("Niezalogowany")
+
+
+def teachersubject(request, subject_id):
+    if request.user.is_authenticated():
+        try:
+            te = Teacher.objects.get(user=request.user)
+            subject = Subject.objects.get(pk=subject_id)
+            students = (subjects.student_id for subjects in subject.subjectsstudents_set.all())
+            return render(request, 'database/teachersubject.html', {'teacher': te, 'subject': subject, 'students': students})
+        except Teacher.DoesNotExist:
+            return HttpResponse("Nie ma nauczyciela")
+        except Subject.DoesNotExist:
+            return HttpResponse("Brak przedmiotu")
+    else:
+        return HttpResponse("Niezalogowany")
+
+def studentsubject(request, subject_id):
+    if request.user.is_authenticated():
+        try:
+            st = Student.objects.get(user=request.user)
+            subject = Subject.objects.get(pk=subject_id)
+            grades = (grade for grade in st.grade_set.all())
+            return render(request, 'database/studentsubject.html', {'student': st, 'subject': subject, 'grades': grades})
+        except Teacher.DoesNotExist:
+            return HttpResponse("Nie ma nauczyciela")
+        except Subject.DoesNotExist:
+            return HttpResponse("Brak przedmiotu")
+    else:
+        return HttpResponse("Niezalogowany")
